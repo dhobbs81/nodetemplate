5344acd3db1c9f3ce7352908fc86baaa3de23f8c
Marcus Hobbs <dhobbs81@gmail.com>
2017-06-15 18:54:03 -0400

    Updated jsdoc documentation. Added docs target to the package.json.

--- src/app.js
+++ src/app.js
@@ -2,28 +2,27 @@
 // Calls npm to start this script; requires a start target in package.json
 
 /**
- * @file Default template of a node application
+ * @module cliapptemplate
+ * @fileOverview Default template of a node application
  * @author Marcus Hobbs
- * @requires babel-cli
- * @requires chalk
- * @requires debug
- * @requires commander
- * Note, depends on npm to execute and babel to transpile from ES6 to
- * something suitable
+ * @requires {@link https://www.npmjs.com/package/babel-cli|babel-cli}
+ * @requires {@link https://www.npmjs.com/package/chalk|chalk}
+ * @requires {@link https://www.npmjs.com/package/debug|debug}
+ * @requires {@link https://www.npmjs.com/package/commander|commander}
+ * @requires {@link https://www.npmjs.com/package/jsdoc|jsdoc}
+ * @version 0.0.1
+ * @license [MIT]{@link https://opensource.org/licenses/MIT}
+ * @see [Babel]{@link https://babeljs.io/} for more information on ES6 transpiling
  */
 
-/**
- * Import a set of generally useful tools
- */
+/** Import a set of generally useful tools */
 import chalk from 'chalk';      // Chalk colorizes console output
 import debug from 'debug';      // Debug provides configurable console output
 var logger = debug('APPNAME');  // The logger outputs debug information
                                 // Replace APPNAME with your APPNAME
 import app from 'commander';    // Provides utils for command line apps
 
-/**
- * Decorate the logger with a signature move
- */
+/** Decorate the logger with a signature move */
 logger.mytag = () => {
     // Define a chalk style to colorize error messages
     let style = chalk.bgRed.bold.yellow;
@@ -31,10 +30,13 @@ logger.mytag = () => {
     logger(style(mytag));
 };
 /**
- * Logs an error message to the console
- *
- * TODO: Make the logger decoratable so we can log to multiple sources
+ * Decorate the logger to output error information
+ * @function
+ * @global
+ * @param {String} text - The error message to stylize
+ * @todo Make the logger decoratable so we can log to multiple sources
  *       dynamically e.g. email, real-time website, database, etc
+ * @returns {String} A stylized string containing the error message
  */
 var error = (text) => {
     // Define a chalk style to colorize error messages
@@ -43,9 +45,13 @@ var error = (text) => {
 };
 
 /**
- * Decorate the logger to output error information
- *
- * @example > logger.error("A BAD THING HAPPENED!");
+ * Logs an error message to the console
+ * @name logError
+ * @function
+ * @global
+ * @param {string} text - The error message to log
+ * @example logger.error("A BAD THING HAPPENED!");
+ * @returns nothing
  */
 logger.error = (text) => {
     logger(error(text));
